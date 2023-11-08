22e4affca9bb40d8ba9f85572e2a557673f3f27d
Ben Hillis <benhill@ntdev.microsoft.com>
2023-11-08 10:04:43 -0800

    Update mode of .X11-unix directory to 01755 (sticky bit set)

--- WSLGd/main.cpp
+++ WSLGd/main.cpp
@@ -301,7 +301,7 @@ try {
     THROW_LAST_ERROR_IF(chmod(c_dbusDir, 0777) < 0);
 
     std::filesystem::create_directories(c_x11RuntimeDir);
-    THROW_LAST_ERROR_IF(chmod(c_x11RuntimeDir, 0777) < 0);
+    THROW_LAST_ERROR_IF(chmod(c_x11RuntimeDir, 01777) < 0);
 
     std::filesystem::create_directories(c_xdgRuntimeDir);
     THROW_LAST_ERROR_IF(chmod(c_xdgRuntimeDir, 0700) < 0);
